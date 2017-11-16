7081543f2226c6608de4baf537a2bbe5dbc57464
vi2233m <31640283+vi2233m@users.noreply.github.com>
2017-11-16 16:11:33 +0800

    Initial commit

---
+++ src/main/java/com/jmeter/JmeterAutotest/App.java
@@ -0,0 +1,14 @@
+package com.jmeter.JmeterAutotest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+}
